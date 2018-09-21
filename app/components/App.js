5d87bb34b0cc32790a2f1a2a6742b49a0fafcd92
Jorge <jorge.groenke@gmail.com>
2018-09-21 14:55:09 +0000

    Add App and Header components

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var Header = require('./Header');
+
+class App extends React.Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+module.exports = App;
---
+++ app/components/Header.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+function Header() {
+  return (
+    <header className='container'>
+      <div className='logo'>
+        VeganEats
+      </div>
+    </header>
+  );
+}
+
+module.exports = Header;
